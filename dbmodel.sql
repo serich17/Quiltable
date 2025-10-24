6e6210bdbeaf3c2639eee9ad7e3f1daf4a1a555d
serich17 <91927166+serich17@users.noreply.github.com>
2025-10-24 15:26:28 -0600

    Added player characters, without implementation

---
+++ dbmodel.sql
@@ -0,0 +1,70 @@
+
+-- ------
+-- BGA framework: Gregory Isabelli & Emmanuel Colin & BoardGameArena
+-- Quiltable implementation : © <Your name here> <Your email address here>
+-- 
+-- This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
+-- See http://en.boardgamearena.com/#!doc/Studio for more information.
+-- -----
+
+-- dbmodel.sql
+
+-- This is the file where you are describing the database schema of your game
+-- Basically, you just have to export from PhpMyAdmin your table structure and copy/paste
+-- this export here.
+-- Note that the database itself and the standard tables ("global", "stats", "gamelog" and "player") are
+-- already created and must not be created here
+
+-- Note: The database schema is created from this file when the game starts. If you modify this file,
+--       you have to restart a game to see your changes in database.
+
+-- Example 1: create a standard "card" table to be used with the "Deck" tools (see example game "hearts"):
+
+-- CREATE TABLE IF NOT EXISTS `card` (
+--   `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+--   `card_type` varchar(16) NOT NULL,
+--   `card_type_arg` int(11) NOT NULL,
+--   `card_location` varchar(16) NOT NULL,
+--   `card_location_arg` int(11) NOT NULL,
+--   PRIMARY KEY (`card_id`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+-- Example 2: add a custom field to the standard "player" table
+-- ALTER TABLE `player` ADD `player_my_custom_field` INT UNSIGNED NOT NULL DEFAULT '0';
+
+
+--  CREATE TABLE IF NOT EXISTS `player_board` (
+--   `board_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+--   `card_type_arg` int(11) NOT NULL,
+--   `card_location` varchar(16) NOT NULL,
+--   `player_id` int(11) NOT NULL,
+--   PRIMARY KEY (`board_id`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+CREATE TABLE IF NOT EXISTS `card` (
+  `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `card_type` varchar(16) NOT NULL,
+  `card_type_arg` int(11) NOT NULL,
+  `card_location` varchar(16) NOT NULL,
+  `card_location_arg` int(11) NOT NULL,
+  PRIMARY KEY (`card_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `return_blocks` (
+    `id` INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `card_id` INT(10) NOT NULL,
+    `location` VARCHAR(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `animation` (
+    `id` INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `card_id` INT(10) NOT NULL,
+    `target` VARCHAR(32) NOT NULL,
+    `loc` INT NOT NULL,
+    `flip` INT NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+ALTER TABLE `player` ADD `endTriggered` TINYINT NOT NULL DEFAULT '0',
+ADD `assistant` INT NOT NULL DEFAULT 0;
